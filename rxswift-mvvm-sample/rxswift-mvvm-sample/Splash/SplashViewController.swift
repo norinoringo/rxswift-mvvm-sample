fa19199978cab0cdb0ca96ffb30d77bf40131cfb
HSaito <hisanoridevelop@gmail.com>
2022-07-24 19:47:39 +0900

    SplashViewModelを追加した

--- rxswift-mvvm-sample/rxswift-mvvm-sample/Splash/SplashViewController.swift
+++ rxswift-mvvm-sample/rxswift-mvvm-sample/Splash/SplashViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class SplashViewController:UIViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
         print("SplashView画面")
     }
 }
---
+++ rxswift-mvvm-sample/rxswift-mvvm-sample/Splash/SplashViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewModel.swift
+//  rxswift-mvvm-sample
+//  
+//  Created by hisanori on 2022/07/23.
+//  
+
+
+import Foundation
+
+class SplashViewModel {
+    
+}
