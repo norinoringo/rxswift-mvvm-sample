c1747ddaa579cc483d5d1c0abf9486437b19db54
HSaito <hisanoridevelop@gmail.com>
2022-07-29 08:54:17 +0900

    RxSwiftを使ってTableViewを作成した

--- rxswift-mvvm-sample/rxswift-mvvm-sample/Top/TopViewController.swift
+++ rxswift-mvvm-sample/rxswift-mvvm-sample/Top/TopViewController.swift
@@ -5,28 +5,48 @@
 //  Created by hisanori on 2022/07/24.
 //  
 
-
-import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class TopViewController:BaseViewController {
     typealias ViewModel = TopViewModel
     typealias TableData = ViewModel.CellData
 
     @IBOutlet weak var tableView: UITableView!
-    
-    var tableData = [TableData(functionName: "GitHubリポジトリ検索")]
+
+    let tableData = Observable<[TableData]>.just([TableData(functionName: "GitHubリポジトリ検索"),
+                                                  TableData(functionName: "GitHub人気リポジトリ表示")])
+
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
         configureNavigationBar()
+        bind()
+    }
+
+    private func bind() {
+        tableData.bind(to: tableView.rx.items(cellIdentifier: "TopViewCell",
+                                              cellType: TopViewCell.self)) { row, element, cell in
+            cell.configure(cellData: element)
+        }
+        .disposed(by: disposeBag)
+
+        tableView.rx.itemSelected
+            .subscribe(onNext: { indexPath in
+                print(indexPath.row)
+            })
+            .disposed(by: disposeBag)
+
+        self.tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
     }
 
     private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UINib(nibName: "TopViewCell", bundle: nil),
+        tableView.register(UINib(nibName: "TopViewCell",
+                                 bundle: nil),
                            forCellReuseIdentifier: "TopViewCell")
     }
 
@@ -36,26 +56,8 @@ class TopViewController:BaseViewController {
     }
 }
 
-extension TopViewController:UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TopViewCell",
-                                                 for: indexPath) as! TopViewCell
-        cell.configure(cellData: tableData[indexPath.row])
-        return cell
-    }
-}
 
 extension TopViewController:UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-    }
-}
-
-extension TopViewController {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 40
     }
