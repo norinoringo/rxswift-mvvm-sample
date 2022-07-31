07fdfedbd35aa52074770aabdc5826cabf395ee5
HSaito <hisanoridevelop@gmail.com>
2022-07-31 18:29:45 +0900

    Modelの定義をViewModelクラスに記載した

--- rxswift-mvvm-sample/rxswift-mvvm-sample/SearchRepository/SearchRepositoryViewModel.swift
+++ rxswift-mvvm-sample/rxswift-mvvm-sample/SearchRepository/SearchRepositoryViewModel.swift
@@ -9,6 +9,22 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+struct SearchRepositoryResponseModel {
+    var name: String?
+    var url: String?
+    var description: String?
+    var stargazersCount: Int?
+    var language: String?
+
+    init(name: String?, url: String?, description: String?, stargazersCount: Int?, language: String?) {
+        self.name = name
+        self.url = url
+        self.description = description
+        self.stargazersCount  = stargazersCount
+        self.language = language
+    }
+}
+
 class SearchRepositoryViewModel {
 
     init(usecase: FetchRepositoryProtocol = FetchRepositoryUseCase()) {
